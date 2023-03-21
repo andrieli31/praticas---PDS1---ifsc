e30703bdd32c54ca436136614f8aad1f6659ccdb
andri <andrieli.m31@aluno.ifsc.edu.br>
2023-03-21 19:05:29 -0300

    ex5

--- lista1/src/ex5/Exercicio5.java
+++ lista1/src/ex5/Exercicio5.java
@@ -1,6 +1,8 @@
 package ex5;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -8,24 +10,18 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
-import java.awt.Color;
-import java.awt.SystemColor;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.Font;
 import javax.swing.JButton;
-import javax.swing.SwingConstants;
+import java.awt.Font;
 
-public class Exercicio5 extends JFrame {
+public class Exe5 extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textFieldLado1;
-	private JTextField textFieldLado2;
-	private JTextField textFieldLado3;
-	private JLabel lbll1;
-	private JLabel lbll2;
-	private JLabel lbll3;
+	private JTextField textL1;
+	private JTextField textL2;
+	private JTextField textL3;
+	private JButton btnBotao;
+	private int num1;
+	private int num2;
 
 	/**
 	 * Launch the application.
@@ -34,7 +30,7 @@ public class Exercicio5 extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Exercicio5 frame = new Exercicio5();
+					Exe5 frame = new Exe5();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -46,80 +42,74 @@ public class Exercicio5 extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Exercicio5() {
+	public Exe5() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(64, 128, 128));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		textL1 = new JTextField();
+		textL1.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		textL1.setBounds(309, 11, 86, 20);
+		contentPane.add(textL1);
+		textL1.setColumns(10);
+		
+		textL2 = new JTextField();
+		textL2.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		textL2.setBounds(309, 46, 86, 20);
+		contentPane.add(textL2);
+		textL2.setColumns(10);
+		
+		textL3 = new JTextField();
+		textL3.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		textL3.setBounds(309, 77, 86, 20);
+		contentPane.add(textL3);
+		textL3.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Informe o primeiro lado do triângulo: ");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		lblNewLabel.setBounds(34, 14, 265, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Informe o segundo lado do triângulo: ");
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		lblNewLabel_1.setBounds(34, 49, 265, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Informe o terceiro lado do triângulo: ");
+		lblNewLabel_2.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		lblNewLabel_2.setBounds(34, 80, 265, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		btnBotao = new JButton("VERIFICAÇÃO");
+		btnBotao.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		btnBotao.setBounds(137, 141, 162, 23);
+		contentPane.add(btnBotao);
+		
+		btnBotao.addActionListener(new ActionListener() {
 
-		textFieldLado3 = new JTextField();
-		textFieldLado3.setBounds(197, 139, 86, 20);
-		contentPane.add(textFieldLado3);
-		textFieldLado3.setColumns(10);
-
-		textFieldLado2 = new JTextField();
-		textFieldLado2.setBounds(197, 94, 86, 20);
-		contentPane.add(textFieldLado2);
-		textFieldLado2.setColumns(10);
-
-		textFieldLado1 = new JTextField();
-		textFieldLado1.setBounds(197, 50, 86, 20);
-		contentPane.add(textFieldLado1);
-		textFieldLado1.setColumns(10);
-
-		JLabel lblVerificaTriangulo = new JLabel("Verificação de triângulos ");
-		lblVerificaTriangulo.setHorizontalAlignment(SwingConstants.CENTER);
-		lblVerificaTriangulo.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
-		lblVerificaTriangulo.setBounds(100, 11, 239, 14);
-		contentPane.add(lblVerificaTriangulo);
-
-		lbll1 = new JLabel("Informe a primeira medida");
-		lbll1.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		lbll1.setBounds(38, 52, 149, 14);
-		contentPane.add(lbll1);
-
-		lbll2 = new JLabel("Informe a segunda medida");
-		lbll2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		lbll2.setBounds(38, 97, 149, 14);
-		contentPane.add(lbll2);
-
-		lbll3 = new JLabel("Informe a terceira medida");
-		lbll3.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		lbll3.setBounds(38, 142, 149, 14);
-		contentPane.add(lbll3);
-
-		JButton btnResult = new JButton("Verificação");
-		btnResult.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnResult.addActionListener(new ActionListener() {
-
+			@Override
 			public void actionPerformed(ActionEvent e) {
-				Double med1 = Double.parseDouble(textFieldLado1.getText());
-				Double med2 = Double.parseDouble(textFieldLado2.getText());
-				Double med3 = Double.parseDouble(textFieldLado3.getText());
+				int num1 = Integer.parseInt(textL1.getText());
+				int num2 = Integer.parseInt(textL2.getText());
+				int num3 = Integer.parseInt(textL3.getText());
 				
-				 
-
-				if (Double.compare(med1, med2) == 0 && Double.compare(med1, med3) == 0
-						&& Double.compare(med2, med3) == 0) {
+				
+				if(num1 == num2 && num1 == num3 && num3 == num2) {
 					JOptionPane.showMessageDialog(null, "Triângulo equilátero");
-				} else if (Double.compare(med1, med2) == 0 || Double.compare(med1, med3) == 0
-						|| Double.compare(med2, med3) == 0 || Double.compare(med3, med2) == 0) {
-					JOptionPane.showMessageDialog(null, "Triângulo isósceles");
-				
-				} else {
-					JOptionPane.showMessageDialog(null, "Triângulo escaleno");
-
+				} if (num1 != num2 && num1 != num3 && num2 != num3) {
+					JOptionPane.showMessageDialog(null, "Triângulo Escaleno");
+				}else {
+					JOptionPane.showMessageDialog(null, "Triângulo Isósceles");
 				}
-
+				
+				
 			}
-
+			
 		});
-		btnResult.setBounds(157, 227, 89, 23);
-
-		contentPane.add(btnResult);
 	}
+
 }
